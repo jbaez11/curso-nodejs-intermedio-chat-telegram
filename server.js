f3dd2329578d357500290b4ee0bb8a96ea40495e
Jonathan Baez <jbaez9910@gmail.com>
2021-01-27 21:47:53 -0500

    Peticiones

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function(req, res){
-    res.send('Hola');
+app.use(router);
+
+router.get('/message', function(req,res){
+    res.send('Lista de mensajes');
 });
+
+router.post('/message', function(req,res){
+    res.send('Mensaje Añadido');
+});
+
+router.delete('/message', function(req,res){
+    res.send('Mensaje Borrado');
+});
+// app.use('/', function(req, res){
+//     res.send('Hola');
+// });
 
 app.listen(3000);
 
