826836f59ddc66768d04c9418064f322397227bb
Jonathan Baez <jbaez9910@gmail.com>
2021-01-27 22:56:24 -0500

    mejorar el sistema de respuestas

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const response = require('./network/response');
 const router = express.Router();
 
  var app = express();
@@ -13,20 +14,28 @@ router.get('/message', function(req,res){
     res.header({
         "custom-header":"nuestro valor personalizado"
     })
-    res.send('Lista de mensajes');
+    //res.send('Lista de mensajes');
+    response.success(req,res, 'Lista de mensajes');
 });
 
 router.delete('/message', function(req,res){
-    res.send('Mensaje eliminado');
+    //res.send('Mensaje eliminado');
+    response.success(req,res, 'Mensaje eliminado');
 });
 
 router.post('/message', function(req,res){
     console.log(req.query);
+    if(req.query.error =='ok'){
+        response.error(req,res, 'Mensaje NO creado correctamente', 400);
+    }else{
+         response.success(req,res, 'Mensaje creado correctamente', 201);
+    }
     console.log(req.body);
-    res.status(201).send({
-        error: '',
-        body: 'Creado correctamente'
-    });
+    // res.status(201).send({
+    //     error: '',
+    //     body: 'Creado correctamente'
+    // });
+   
     //res.send('Mensaje ' + req.body.text + 'añadido correctamente');
 });
 // app.use('/', function(req, res){
