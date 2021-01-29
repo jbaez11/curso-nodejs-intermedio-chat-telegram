9c075523d01b9c6ed5240b7d3f7daaac45bf595a
Jonathan Baez <jbaez9910@gmail.com>
2021-01-29 00:13:32 -0500

    leer y almacenar datos mongoDB

--- components/message/store.js
+++ components/message/store.js
@@ -1,12 +1,25 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+//mongodb://Hola1099:Hola1099@cluster0-shard-00-00.xurmg.mongodb.net:27017,cluster0-shard-00-01.xurmg.mongodb.net:27017,cluster0-shard-00-02.xurmg.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-b3c9zs-shard-0&authSource=admin&retryWrites=true&w=majority
+
+db.Promise = global.Promise;
+db.connect('mongodb://Hola1099:Hola1099@cluster0-shard-00-00.xurmg.mongodb.net:27017,cluster0-shard-00-01.xurmg.mongodb.net:27017,cluster0-shard-00-02.xurmg.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-b3c9zs-shard-0&authSource=admin&retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true 
+});
+
+console.log('DB conectada con exito');
 
 function addMessage(message){
-    list.push(message);
+   // list.push(message);
+   const myMessage = new Model(message);
+   myMessage.save();
 
 }
 
-function getMessage(){
-    return list;
+async function getMessage(){
+    //return list;
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports= {
