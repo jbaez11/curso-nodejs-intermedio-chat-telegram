564b2bbe6f11ccd31bd1693ceda9e003008b57ac
Jonathan Baez <jbaez9910@gmail.com>
2021-01-28 22:34:32 -0500

    controladores, definicion de la logica

--- components/message/network.js
+++ components/message/network.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const response = require('../../network/response');
+const controller = require('./controller');
 const router = express.Router();
 
 router.get('/', function(req,res){
@@ -17,13 +18,16 @@ router.delete('/', function(req,res){
 });
 
 router.post('/', function(req,res){
-    console.log(req.query);
-    if(req.query.error =='ok'){
-        response.error(req,res, 'Error inesperado', 500, 'Es solo una simulacion de un error');
-    }else{
-         response.success(req,res, 'Mensaje creado correctamente', 201);
-    }
-    console.log(req.body);
+    
+    controller.addMessage(req.body.user, req.body.message)
+        .then((fullMessage)=>{
+            response.success(req,res, fullMessage, 201);
+        })
+        .catch(e => {
+            response.error(req,res, 'Informacion Invalida', 400, 'error en el controller');
+        });
+    
+   
    
 });
 
