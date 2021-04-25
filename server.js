7ea69057d01b4056f95310277841f41615532502
Jonathan Baez <jbaez9910@gmail.com>
2021-04-25 00:03:30 -0500

    imagen de la arquitectura usada

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const socket = require('./socket');
 const db = require('./db');
@@ -10,7 +11,7 @@ const router = require('./network/routes');
 
 db('mongodb://Hola1099:Hola1099@cluster0-shard-00-00.xurmg.mongodb.net:27017,cluster0-shard-00-01.xurmg.mongodb.net:27017,cluster0-shard-00-02.xurmg.mongodb.net:27017/telegrams?ssl=true&replicaSet=atlas-b3c9zs-shard-0&authSource=admin&retryWrites=true&w=majority');
  
- 
+ app.use(cors());
  app.use(bodyParser.json());
  app.use(bodyParser.urlencoded({ extended: false }));
  
@@ -19,8 +20,8 @@ socket.connect(server);
 
 
 app.use('/app', express.static('public'));
-server.listen(3500, function(){
-    console.log('La aplicacion esta escuchando en http://localhost:3500')
+server.listen(3000, function(){
+    console.log('La aplicacion esta escuchando en http://localhost:3000')
 });
 
-console.log('la aplicacion esta escuchando en el puerto 3500');
+console.log('la aplicacion esta escuchando en el puerto 3000');
